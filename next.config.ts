065027eafc64253fbc0472c990ce399524136fdd
Nitesh Jat <njat7249@gmail.com>
2025-10-31 01:31:38 +0530

    Modify pathname settings and add Cloudinary support
    Updated pathname settings for various hosts to allow all paths and added configurations for Cloudinary and a backend service.

--- next.config.ts
+++ next.config.ts
@@ -14,25 +14,38 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'placehold.co',
         port: '',
-        pathname: '/**',
+        pathname: '/',
       },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
         port: '',
-        pathname: '/**',
+        pathname: '/',
       },
       {
         protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
-        pathname: '/**',
+        pathname: '/',
       },
       {
         protocol: 'http',
         hostname: 'localhost',
         port: '5000',
-        pathname: '/api/v1/**',
+        pathname: '/', // Changed from '/api/v1/' to allow all paths
+      },
+      // ✅ ADD THESE TWO FOR CLOUDINARY
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/',
+      },
+      {
+        protocol: 'https',
+        hostname: 'restaurant-backend-w7j5.onrender.com',
+        port: '',
+        pathname: '/',
       },
     ],
   },
